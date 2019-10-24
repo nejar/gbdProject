affc8c17fb3eec6881afa4a8a851ebf9e163bca4
Nejar <Nejar>
2019-10-24 12:32:44 +0545

    managed code alignment

--- login.php
+++ login.php
@@ -26,39 +26,40 @@ if($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['_LOGIN'])) {
 	$_SESSION['login_error'] = 'Invalid username or password';
 } else {
 	unset($_SESSION['login_error']);
-}
+	}
 ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="assets/style.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.js"></script>
-	<script src="assets/main.js"></script>
-</head>
-<body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="assets/style.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.1/dist/jquery.validate.js"></script>
+		<script src="assets/main.js"></script>
+	</head>
+	<body>
 
-<div class="wrapper fadeInDown container">
-  <div id="formContent">
-    <form id="login_form" method="post" action="">
-    	<div class="my-3 text-danger">
-    		<?php
-				if(isset($_SESSION['login_error'])) {
-					echo $_SESSION['login_error'];
-				}
-			?>
-    	</div>
-      <input type="text" id="login" class="fadeIn second" name="username" placeholder="username">
-      <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
-      <input type="submit" class="fadeIn fourth btn btn-primary" name="_LOGIN" value="Log In">
-    </form>
+		<div class="wrapper fadeInDown container">
+		  <div id="formContent">
+		    <form id="login_form" method="post" action="">
+		    	<div class="my-3 text-danger">
+		    		<?php
+						if(isset($_SESSION['login_error'])) {
+							echo $_SESSION['login_error'];
+						}
+					?>
+		    	</div>
+		      <input type="text" id="login" class="fadeIn second" name="username" placeholder="username">
+		      <input type="text" id="password" class="fadeIn third" name="password" placeholder="password">
+		      <input type="submit" class="fadeIn fourth btn btn-primary" name="_LOGIN" value="Log In">
+		    </form>
 
-  </div>
-</div>
-</body>
+		  </div>
+		</div>
+
+	</body>
 </html>
